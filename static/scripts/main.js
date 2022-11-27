7646e45ce36a344d266a14e34d350fdf4166a3f6
rvitality <vital.renzjohn@gmail.com>
2022-11-27 14:45:12 +0800

    Updates

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,14 +1,30 @@
+const timerContainer = document.querySelector(".meditation .timer");
+const timerContentEl = document.querySelector(".meditation .timer .value");
+if (timerContainer) {
+    let timeleft = 59;
+    let downloadTimer = setInterval(() => {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+        }
+        timerContentEl.textContent = timeleft;
+        timeleft -= 1;
+
+        console.log(timeleft);
+    }, 1000);
+
+    // <progress value="0" max="10" id="progressBar"></progress>;
+}
+
 // ! SPEECH TO TEXT
 const speechContainer = document.querySelector("#speech-container");
 const micBtn = document.querySelector(".mic-btn");
 const pulse = document.querySelector(".pulse-ring");
 
-if (speechContainer) {
-    micBtn.addEventListener("click", e => {
-        pulse.classList.toggle("show");
-    });
+// const audioCherryBlossom = document.querySelector("#diary-bg-music");
 
-    let speech = true;
+if (speechContainer) {
+    let speech = false;
+
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
     const recognition = new SpeechRecognition();
@@ -25,8 +41,19 @@ if (speechContainer) {
         document.getElementById("p").innerHTML = transcript;
     });
 
-    if (speech == true) {
-        recognition.start();
-        recognition.addEventListener("end", recognition.start);
-    }
+    recognition.addEventListener("end", recognition.stop);
+
+    micBtn.addEventListener("click", e => {
+        pulse.classList.toggle("show");
+
+        speech = !speech;
+        console.log(speech);
+
+        if (speech) {
+            recognition.start();
+        } else {
+            recognition.abort();
+            recognition.stop();
+        }
+    });
 }
